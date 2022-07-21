cff516cd89418094993e265a42b817507e90876a
noggrie <eunyoung950210@gmail.com>
2022-07-21 16:15:38 +0900

    ✨ [FE] | [#S07P12C204-131] feat : 제목  클릭 > 상세 페이지 이동

--- frontend/src/pages/HistoryDetail.tsx
+++ frontend/src/pages/HistoryDetail.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const HistoryDetail = () => {
-  return <div>히스토리 상세 페이지</div>;
+  const { historyId } = useParams();
+  return (
+    <div>
+      히스토리 {historyId}번 상세 페이지
+      <p>방주인: </p>
+      <p>참여자: </p>
+      <p>일시: </p>
+      <p>로그</p>
+    </div>
+  );
 };
 
 export default HistoryDetail;
