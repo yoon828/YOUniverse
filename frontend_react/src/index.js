3f0b8114b41c76a4f3908b86c1bf671823d0dac0
noggrie <eunyoung950210@gmail.com>
2022-07-26 10:47:42 +0900

    ♻️ [FE] : ts > js

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './common/style/index.scss';
 import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
+// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -17,4 +18,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
