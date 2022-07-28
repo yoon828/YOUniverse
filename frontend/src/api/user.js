b70ab377ea5bd89d23bc4eb46a10484bb7fa26e7
noggrie <eunyoung950210@gmail.com>
2022-07-28 11:51:13 +0900

    ✨ [FE] | feat : 페이지네이션 구현

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,1 +1,9 @@
+import axios from 'axios';
 
+// History test용
+export const jsonPlaceholder = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/'
+});
+export const dumpHistory = async () => {
+  return await jsonPlaceholder.get('comments/');
+};
--- frontend/src/pages/mypage/HistoryList.jsx
+++ frontend/src/pages/mypage/HistoryList.jsx
@@ -1,53 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const dataList = [
-  {
-    owner: 'Germane Wilkins',
-    title: 'Swiętokrzyskie',
-    date: '2022년 8월 13일',
-    historyId: 1
-  },
-  {
-    owner: 'Myra Clemons',
-    title: 'Cartago',
-    date: '2023년 5월 27일',
-    historyId: 2
-  },
-  {
-    owner: 'Orli Reid',
-    title: 'Xīnán',
-    date: '2021년 11월 6일',
-    historyId: 3
-  },
-  {
-    owner: 'Francesca Lang',
-    title: 'Samara Oblast',
-    date: '2023년 6월 9일',
-    historyId: 4
-  },
-  {
-    owner: 'Stephen Mclean',
-    title: 'North-East Region',
-    date: '2021년 10월 12일',
-    historyId: 5
-  }
-];
+import React, { useState, useEffect } from 'react';
+import { dumpHistory } from 'api/user';
+import Page from 'modules/Pagination';
 
 const HistoryList = () => {
-  return (
-    <div>
-      <p>히스토리 내역 페이지</p>
-      <ul>
-        {dataList.map((data) => (
-          <li key={data.owner}>
-            {data.owner} |
-            <Link to={`/history/${data.historyId}`}>{data.title}</Link> |
-            {data.date}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  const [historyList, setHistoryList] = useState([]);
+
+  useEffect(() => {
+    dumpHistory()
+      .then(({ data }) => {
+        setHistoryList(data);
+      })
+      .catch((err) => alert(err));
+  }, []);
+  return <Page data={historyList} />;
 };
 export default HistoryList;
