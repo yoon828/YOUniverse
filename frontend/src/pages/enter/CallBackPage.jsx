ec053bd72fc294a830b144533d71c7291ef95603
seohmoon <walltod@naver.com>
2022-07-30 19:05:13 +0900

    :hammer: [FE] | [S07P12C204-123] fix : 토큰 발급 경로 수정

--- frontend/src/pages/enter/CallBackPage.jsx
+++ frontend/src/pages/enter/CallBackPage.jsx
@@ -2,14 +2,14 @@ import { useLocation } from 'react-router-dom';
 
 const CallBackPage = () => {
   const { search } = useLocation();
-  console.log(useLocation());
+  // console.log(useLocation());
 
   const url = new URLSearchParams(search);
-  console.log('temp', url);
+  // console.log('temp', url);
 
   const accessToken = url.get('accessToken');
   const refreshToken = url.get('refreshToken');
-  console.log(accessToken, refreshToken);
+  // console.log(accessToken, refreshToken);
 
   return (
     <>
