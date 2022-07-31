f408bf8c7562045cee0514f2ccbaee1c4764305a
noggrie <eunyoung950210@gmail.com>
2022-07-31 19:02:01 +0900

    ✨ [FE] | [S07P12C204-160] feat : 비인증 유저 로그인 페이지로..

---
+++ frontend/src/routes/PrivateRoute.js
@@ -0,0 +1,35 @@
+import { Route, Redirect } from 'react-router';
+
+import { useSelector } from 'react-redux';
+import _ from 'lodash';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isLoggedIn = useSelector(
+    (state) => !_.isEmpty(state.auth.value.refreshToken)
+  );
+
+  /* 테스트용
+  console.log('Private', isLoggedIn);
+  console.log('Private', { component: Component, ...rest });
+  console.log('Private-useL', useLocation());
+  */
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location.pathname }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+export default PrivateRoute;
