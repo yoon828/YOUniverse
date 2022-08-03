d77117cdba55bf0e70839201f4576e00fdf9b981
noggrie <eunyoung950210@gmail.com>
2022-08-03 23:20:28 +0900

    🐛 [FE] | fix : 히스토리 상세 조회 api 수정 후 적용

--- frontend/src/api/history.js
+++ frontend/src/api/history.js
@@ -1,8 +1,16 @@
 import { api } from 'api/api';
 
-export const storeHistory = async (content) => {
-  return await api.post('/history', content);
+// 로그 생성
+export const storeHistory = async (payload) => {
+  return await api.post('/history', payload);
 };
+
+// 전체 로그 조회
 export const getHistoryList = async () => {
   return await api.get('/history/list');
+};
+
+// 단일 로그 조회
+export const getHistory = async (id) => {
+  return await api.get(`/history/${id}`);
 };
--- frontend/src/pages/mypage/HistoryDetail.jsx
+++ frontend/src/pages/mypage/HistoryDetail.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, Link, useHistory } from 'react-router-dom';
+import { getHistory } from 'api/history';
 
 const HistoryDetail = () => {
   const { historyId } = useParams();
+  const [history, setHistory] = useState({});
+
+  const handleDownLoad = () => {
+    console.log('다운로드됩니다~~');
+  };
+
+  useEffect(() => {
+    getHistory(historyId)
+      // 조회 성공
+      .then(({ data }) => {
+        setHistory(data.data);
+      })
+
+      // 조회 실패
+      .catch((err) => {
+        console.log(err);
+        alert('오류가 발생했습니다. 메인페이지로 돌아갑니다');
+        useHistory.history.replace('/');
+      });
+  }, []);
+
   return (
     <div>
-      히스토리 {historyId}번 상세 페이지
-      <p>방주인: </p>
-      <p>참여자: </p>
-      <p>일시: </p>
-      <p>로그</p>
+      <p>모임제목: {history.roomName}</p>
+      <p>방주인: {history.hostName}</p>
+      <p>참여자: {history.participants}</p>
+      <p>일시: {history.date}</p>
+      <p>로그: {history.filePath}</p>
+      <button onClick={handleDownLoad}>다운로드</button>
+      <Link to="/history">목록</Link>
     </div>
   );
 };
