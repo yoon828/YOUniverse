d8c44ac225ceda86d66160566f26e61dd1ba0cbb
noggrie <eunyoung950210@gmail.com>
2022-08-03 23:10:15 +0900

    ✨ [FE] | [S07P12C204-71, 86, 121] feat : 문의 전체 내역 조회 페이지 구현

--- frontend/src/pages/mypage/QnAList.jsx
+++ frontend/src/pages/mypage/QnAList.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getQnAList } from 'api/qna';
+import Page from 'modules/Pagination';
 
 const QnAList = () => {
-  return <div>1:1 문의내역</div>;
-};
+  const [qnaList, setQnAList] = useState([]);
 
+  useEffect(() => {
+    getQnAList()
+      .then(({ data }) => {
+        console.log(data.data);
+        setQnAList(data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>1:1 문의내역</h1>
+      <Page
+        type="/question"
+        data={qnaList}
+        items={['title', 'question_date', 'isAnswered']}
+      />
+    </div>
+  );
+};
 export default QnAList;
