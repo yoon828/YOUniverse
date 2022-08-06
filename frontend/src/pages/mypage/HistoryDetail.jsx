88d9a02da441ef7d8074d5fe66ee050a840cf37d
noggrie <eunyoung950210@gmail.com>
2022-08-06 21:36:37 +0900

    🩹 [FE] fix : 히스토리 디테일 api 오류 시 메인페이지로 이동 버그 수정

--- frontend/src/pages/mypage/HistoryDetail.jsx
+++ frontend/src/pages/mypage/HistoryDetail.jsx
@@ -5,7 +5,8 @@ import { transform } from 'common/functions/functions';
 
 const HistoryDetail = () => {
   const { historyId } = useParams();
-  const [history, setHistory] = useState({});
+  const [historyItem, setHistoryItem] = useState({});
+  const history = useHistory();
 
   const handleDownLoad = () => {
     console.log('다운로드됩니다~~');
@@ -15,24 +16,24 @@ const HistoryDetail = () => {
     getHistory(historyId)
       // 조회 성공
       .then(({ data }) => {
-        setHistory(data.data);
+        setHistoryItem(data.data);
       })
 
       // 조회 실패
       .catch((err) => {
         console.log(err);
         alert('오류가 발생했습니다. 메인페이지로 돌아갑니다');
-        useHistory.history.replace('/');
+        // history.replace('/');
       });
   }, []);
 
   return (
     <div className="page_container history_detail">
       <p className="title">모임제목: {history.roomName}</p>
-      <p>방주인: {history.hostName}</p>
-      <p>참여자: {history.participants}</p>
-      <p>일시: {transform(history.date)}</p>
-      <p>로그: {history.filePath}</p>
+      <p>방주인: {historyItem.hostName}</p>
+      <p>참여자: {historyItem.participants}</p>
+      <p>일시: {transform(historyItem.date)}</p>
+      <p>로그: {historyItem.filePath}</p>
       <button onClick={handleDownLoad}>다운로드</button>
       <Link to="/history">목록</Link>
     </div>
