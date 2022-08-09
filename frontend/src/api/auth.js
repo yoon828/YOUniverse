fa41a5efaf852ef75eb34d6bafc346ce0b27577b
noggrie <eunyoung950210@gmail.com>
2022-08-09 14:00:36 +0900

    💡 [FE] | style : loginUser, refreshToken API 주석처리

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,13 +1,13 @@
 import { api } from 'api/api';
-import { getRefreshToken } from 'common/functions/functions';
+// import { getRefreshToken } from 'common/functions/functions';
 
-export const loginUser = async (user, success, fail) => {
-  return await api.post('/api/auth/login', user);
-};
+// export const loginUser = async (user, success, fail) => {
+//   return await api.post('/api/auth/login', user);
+// };
 
-export const renewToken = async () => {
-  return await api.get(`/token/reissuance/${getRefreshToken()}`);
-};
+// export const renewToken = async () => {
+//   return await api.get(`/token/reissuance/${getRefreshToken()}`);
+// };
 
 export const logoutUser = async () => {
   return await api.get('/logout');
