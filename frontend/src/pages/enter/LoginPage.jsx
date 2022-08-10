64bfb5eca018f1c515c530bc332ab4bfb0c1d026
김윤민 <k915k1004@naver.com>
2022-08-11 00:12:41 +0900

    💄[FE] | style : login 페이지 css 수정

--- frontend/src/pages/enter/LoginPage.jsx
+++ frontend/src/pages/enter/LoginPage.jsx
@@ -15,17 +15,19 @@ const Login = () => {
     <div className="login_page">
       <div className="main_logo">
         <Link to="/">
-          <img
-            src="https://blog.kakaocdn.net/dn/be0xab/btrHTW8GtRk/LDOhwqWEBUDFkVh1S5aNv0/img.png"
-            alt="logo"
-          />
+          <img src="asset/img/logo.png" alt="logo" className="logo_img" />
         </Link>
       </div>
       <div className="login_box">
-        <div className="login_box_text">지금 너, 나, 우리</div>
-        <div className="login_kakao">
-          <a href={KAKAO_AUTH_URL}>카카오 로그인</a>
+        <div className="login_box_text">
+          또 다른 세상, <br /> YOUniverse
         </div>
+        <a href={KAKAO_AUTH_URL} className="login_kakao">
+          <div className="login_kakao_div">
+            <img src="/asset/img/main/kakao_login.png" alt="카카오로그인" />
+          </div>
+        </a>
+        <img src="/asset/img/main/saturn.png" alt="행성" className="saturn" />
       </div>
     </div>
   );
