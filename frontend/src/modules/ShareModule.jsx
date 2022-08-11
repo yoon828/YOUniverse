f11d505f3c5f8a2288c78f80cb5fa48aed30497e
문서희 <shhhm@hanmail.net>
2022-08-11 09:52:57 +0900

    :truck: [FE] | style : ShareModule url 수정

--- frontend/src/modules/ShareModule.jsx
+++ frontend/src/modules/ShareModule.jsx
@@ -5,11 +5,15 @@ import { useSelector } from 'react-redux';
 const ShareModule = () => {
   const { name } = useSelector((state) => state.user.value);
   const API_KEY = 'e09419333dd810c5a8fcc8db0d0c8aea';
+  const storeSessionId = useSelector((state) => state.user.value.sessionId);
+  console.log(storeSessionId);
+  const storeName = useSelector((state) => state.user.value.name);
+  console.log(storeName);
+  const url = `http://cjswltjr.shop:3000/invite/${storeSessionId}/${storeName}`;
 
   const onCopyUrl = () => {
     const textarea = document.createElement('textarea');
     document.body.appendChild(textarea);
-    const url = window.document.location.href;
     textarea.value = url;
     textarea.select();
     document.execCommand('copy');
@@ -27,7 +31,7 @@ const ShareModule = () => {
   };
 
   const onShareKakaoClick = () => {
-    shareKakaoLink(name, 'https://naver.com');
+    shareKakaoLink(name, url);
   };
 
   useEffect(() => {
