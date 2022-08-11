f0997741b9253caa719e132c864ef9dc036b0181
문서희 <shhhm@hanmail.net>
2022-08-11 09:15:11 +0900

    :truck: [FE] | style : rename  SharePage storeName

--- frontend/src/pages/enter/SharePage.jsx
+++ frontend/src/pages/enter/SharePage.jsx
@@ -8,13 +8,13 @@ import './SharePage.scss';
 const Share = () => {
   const storeSessionId = useSelector((state) => state.user.value.sessionId);
   console.log(storeSessionId);
-  const storename = useSelector((state) => state.user.value.name);
-  console.log(storename);
+  const storeName = useSelector((state) => state.user.value.name);
+  console.log(storeName);
   return (
     <div className="share">
       <h1>공유하기</h1>
       <ShareModule />
-      <Link to={`/room/${storeSessionId}/${storename}`}>다음</Link>
+      <Link to={`/room/${storeSessionId}/${storeName}`}>다음</Link>
     </div>
   );
 };
