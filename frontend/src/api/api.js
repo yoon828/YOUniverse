8c0f1a2f6fa87f1463e00a4129baec32f04808d1
0ej4613 <0ej4613@naver.com>
2022-08-15 11:25:23 +0900

    feat : baseURL 수정

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,7 +5,7 @@ import {
   setAccessToken
 } from 'common/functions/functions';
 
-const baseURL = 'http://i7c204.p.ssafy.io:8080';
+const baseURL = 'http://i7c204.p.ssafy.io:8080/api/back';
 
 // 기본 api
 export const api = axios.create({
