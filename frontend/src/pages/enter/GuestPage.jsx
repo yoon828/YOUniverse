7560e1df3b6d8bf8ff5707e829699d8e049855da
문서희 <shhhm@hanmail.net>
2022-08-24 00:08:02 +0900

    ✨:lipstick: [FE] | style : GuestPage title 수정

--- frontend/src/pages/enter/GuestPage.jsx
+++ frontend/src/pages/enter/GuestPage.jsx
@@ -46,7 +46,7 @@ const Guest = () => {
           className="astronaut_big_img"
         />
         <form className="guest_form">
-          <h1 className="title">이름을 입력해주세요.</h1>
+          <h1 className="title">이름을 입력해주세요</h1>
           <input
             type="text"
             name="guest"
